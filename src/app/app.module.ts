ce62f8aacd8d3e1ba7d5c210f53399a6a490835c
Irina Ferreira <irina.f@aluno.ifsp.edu.br>
2023-05-24 21:04:44 -0300

    feat: add navigation routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import { FormsModule } from '@angular/forms';
+
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -21,7 +22,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
         AppRoutingModule,
         FormsModule
     ],
-  providers: [],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
